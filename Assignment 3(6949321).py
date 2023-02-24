18d3ed5a63bd8beeeeede152f8117d1565c92edf
mawulicodes <125625857+mawulicodes@users.noreply.github.com>
2023-02-24 12:10:18 -0300

    Add files via upload

---
+++ Assignment 3(6949321).py
@@ -0,0 +1,82 @@
+#https://github.com/mawulicodes/CIVIL-ENGINEERING-YEAR-2-2023-.git
+#prgramming Assignment 3
+#This programe is to show the cars available at Mawuli Cars and their Respective prises.
+a='Toyota Corolla'
+b='Lamborghini'
+c='Porsche'
+d='Mercedes-Benz'
+e='Ford'
+f='Hyundai'
+g='Luxus'
+h='Jeep'
+i='BMW'
+j='Ferrari'
+k='Bentley'
+l='Peugeot'
+m='Afra Romeo'
+n='Honda'
+o='GMC'
+p='Cadillac'
+q='Chevrolet'
+r='Kia'
+s='Mini'
+t='Mazda'
+car=input('please type the name of the car you are interested in:' )
+if car==a:
+    print('Awesome!! we have Toyota Corolla \n The price is $70000')
+elif car==b:
+    print('Awesome!! we have Lamborghini \n The price is $90000')
+elif car==c:
+    print('Awesome!! we have Porsche \n The price is $60000')
+elif car==d:
+    print('Awesome!! we have Mercedes-Benz \n The price is $70000')
+elif car==e:
+    print('Awesome!! we have Ford \n The price is $50000')
+elif car==f:
+    print('Awesome!! we have Hyundai \n The price is $80000')
+elif car==g:
+    print('Awesome!! we have Luxus \n The price is $100000')
+elif car==h:
+    print('Awesome!! we have Jeep \n The price is $640000')
+elif car==i:
+    print('Awesome!! we have BMW \n The price is $1110000')
+elif car==j:
+    print('Awesome!! we have Ferrari \n The price is $880000')
+elif car==k:
+    print('Awesome!! we have Bentley \n The price is $220000')
+elif car==l:
+    print('Awesome!! we have Peageot \n The price is $70000')
+elif car==m:
+    print('Awesome!! we have Afra Romeo \n The price is $540000')
+elif car==n:
+    print('Awesome!! we have Honda \n The price is $70000')
+elif car==o:
+    print('Awesome!! we have GMC \n The price is $770000')
+elif car==p:
+    print('Awesome!! we have Cadillac \n The price is $570000')
+elif car==q:
+    print('Awesome!! we have Chevrolet \n The price is $370000')
+elif car==r:
+    print('Awesome!! we have Kia \n The price is $270000')
+elif car==s:
+    print('Awesome!! we have Mini \n The price is $80000')
+elif car==t:
+    print('Awesome!! we have Mazda \n The price is $570000')
+else:
+    print('Sorry, this type of car is not available at the moment. \n for more information please contact Mawuli Cars on +2333550620713')
+    
+    
+    
+    
+    
+    
+     
+    
+    
+    
+    
+    
+    
+    
+    
+    
